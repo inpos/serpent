9b3980289bb31bb8dc3e68430384be3b1e248ec2
inpos <inpos@yandex.ru>
2016-05-25 16:18:01 +0300

    ошибка комментария

--- serpent/config.py
+++ serpent/config.py
@@ -8,7 +8,7 @@ conf.SRVNAME = 'Serpent'
 conf.srv_version = '%s %s' % (conf.SRVNAME, conf.VERSION)
 conf.local_domains = ['dom.lan']    # Список доменов, для которых будет приниматься почта
 conf.tls = True
-conf.tls_pem = '/home/inpos/ownCloud/workspace/serpent/serpent.pem'
+conf.tls_pem = '/home/inpos/Программы/git/pydev/serpent/serpent.pem'
 conf.smtp_open_relay = False        # Разрешить ли пересылку откуда угодно куда угодно
 conf.smtp_email_delim = '@'
 conf.smtp_header = '''from [{sender_ip}] (helo={sender_host})
@@ -21,7 +21,7 @@ conf.smtp_hostname = 'mail.dom.lan'
 conf.app_dir = '/home/inpos/tmp/serpent'
 conf.smtp_queue_dir = 'smtp_queue'
 conf.smtp_message_size = 40                 # Размер в МБ
-conf.smtp_queue_check_period = 30           # Передичность запуска обработки очереди в минутах
+conf.smtp_queue_check_period = 30           # Период запуска обработки очереди в минутах
 conf.smtp_queue_message_ttl = 3 * 24 * 60   # Время жизни сообщения в очереди в минутах
 conf.maildir_user_path = 'mailstore/%s/'
 
