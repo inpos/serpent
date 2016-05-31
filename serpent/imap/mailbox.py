f36515f2334882aa2f9167c2ffc68b01d3a29ba2
inpos <inpos@yandex.ru>
2016-05-31 13:42:31 +0300

    ExtendedMaildir

---
+++ serpent/imap/mailbox.py
@@ -0,0 +1,11 @@
+from mailbox import Maildir
+
+class ExtendedMaildir(Maildir):
+    def set_flags(self, key):
+        pass
+    def get_flags(self, key):
+        pass
+    def add_flag(self):
+        pass
+    def remove_flag(self):
+        pass
